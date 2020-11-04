f991bb4cc7ae5cb975ae175abe0c58563ab6e70e
Christopher <christo.navas@hotmail.fr>
2020-11-04 15:59:49 +0100

    improve css for leaderboardpage

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -8,7 +8,7 @@
         Entrez votre pseudo pour commencer
       </h2>
         <q-form class="q-pt-md" style="max-width: 250px; margin:auto" @submit="onSubmit" @reset="onReset">
-          <q-input class="q-mb-xl" outlined  bg-color="white" label="Pseudo" v-model="text" input-class="text-center" />
+          <q-input class="q-mb-xl custom-input" outlined  bg-color="white" label="Pseudo" v-model="text" input-class="text-center" />
           <q-btn to="quiz" color="secondary" label="Jouer" size="20px"  class="full-width" />
         </q-form>
       </div>
--- src/pages/LeaderboardPage.vue
+++ src/pages/LeaderboardPage.vue
@@ -1,59 +1,29 @@
 <template>
-  <q-page class="flex column items-center justify-center">
+  <q-page class="flex column justify-center">
     <!-- content -->
     <div class="text-center">
       <h1>Leaderboard</h1>
-      <q-select
-        label="Afficher"
-        v-model="model"
-        :options="options"
-        @input="(val) => { onValueChange(val); }"
-        class="q-pb-xl"
-      />
-      <q-table
-        :data="data.popcorn"
-        :columns="columns"
-        row-key="name"
-        v-if="showPopcorn"
-        hide-bottom
-        virtual-scroll
-        :pagination.sync="pagination"
-      />
-      <q-table
-        :data="data.moviz"
-        :columns="columns"
-        row-key="name"
-        v-if="showMoviz"
-        hide-bottom
-        virtual-scroll
-        :pagination.sync="pagination"
-      />
-      <q-table
-        :data="data.wsfquiz"
-        :columns="columns"
-        row-key="name"
-        v-if="showWSFquiz"
-        hide-bottom
-        virtual-scroll
-        :pagination.sync="pagination"
-      />
-      <q-table
-        :data="data.cinequiz"
-        :columns="columns"
-        row-key="name"
-        v-if="showCinequiz"
-        hide-bottom
-        virtual-scroll
-        :pagination.sync="pagination"
-      />
+      <div class="items-baseline flex justify-center">
+        <div class="text-h5 text-white text-bold white q-pr-lg">
+          Afficher :
+        </div>
+        <div class="">
+          <q-select  v-model="theme" bg-color="white" outlined :options="options" @input="(val) => { onValueChange(val); }"
+            class="custom-select q-pb-xl" />
+        </div>
+      </div>
+      <div style="max-width: 600px;margin: auto;">
+        <q-table :data="data.popcorn" :columns="columns" row-key="name" v-if="showPopcorn" hide-bottom virtual-scroll
+          :pagination.sync="pagination" />
+        <q-table :data="data.moviz" :columns="columns" row-key="name" v-if="showMoviz" hide-bottom virtual-scroll
+          :pagination.sync="pagination" />
+        <q-table :data="data.wsfquiz" :columns="columns" row-key="name" v-if="showWSFquiz" hide-bottom virtual-scroll
+          :pagination.sync="pagination" />
+        <q-table :data="data.cinequiz" :columns="columns" row-key="name" v-if="showCinequiz" hide-bottom virtual-scroll
+          :pagination.sync="pagination" />
+        </div>
       <div class="q-pt-xl">
-      <q-btn
-        to="/"
-        color="secondary"
-        label="Rejouer"
-        size="20px"
-        style="width: 250px"
-      />
+        <q-btn to="/" color="secondary" label="Rejouer" size="20px" style="width: 250px" />
       </div>
     </div>
   </q-page>
@@ -93,7 +63,7 @@ export default {
   },
   data () {
     return {
-      model: null,
+      theme: 'Popcorn',
       showPopcorn: true,
       showMoviz: false,
       showWSFquiz: false,
@@ -102,172 +72,173 @@ export default {
         rowsPerPage: 0
       },
       options: ['Popcorn', 'Moviz', 'WSFquiz', 'Cinéquiz'],
-      columns: [
-        {
-          name: 'ranking',
-          align: 'left',
-          label: 'RANKING #',
-          field: 'ranking',
-          sortable: true
-        },
-        { name: 'id', align: 'left', label: 'ID', field: 'id', sortable: true },
-        {
-          name: 'score',
-          align: 'left',
-          label: 'SCORE (%)',
-          field: 'score',
-          sortable: true,
-          sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
-        }
+      columns: [{
+        name: 'ranking',
+        align: 'left',
+        label: 'RANKING #',
+        field: 'ranking',
+        sortable: true
+      },
+      {
+        name: 'id',
+        align: 'left',
+        label: 'ID',
+        field: 'id',
+        sortable: true
+      },
+      {
+        name: 'score',
+        align: 'left',
+        label: 'SCORE (%)',
+        field: 'score',
+        sortable: true,
+        sort: (a, b) => parseInt(a, 10) - parseInt(b, 10)
+      }
       ],
       data: {
-        popcorn: [
-          {
-            ranking: 1,
-            id: 'Devon Lane',
-            score: '100'
-          },
-          {
-            ranking: 2,
-            id: 'Cody Fisher',
-            score: '90'
-          },
-          {
-            ranking: 3,
-            id: 'Jacob Jones',
-            score: '80'
-          },
-          {
-            ranking: 4,
-            id: 'Robert Fox',
-            score: '75'
-          },
-          {
-            ranking: 5,
-            id: 'Eleanor Pena',
-            score: '70'
-          },
-          {
-            ranking: 6,
-            id: 'Kathryn Murphy',
-            score: '50'
-          },
-          {
-            ranking: 687,
-            id: 'John Doe',
-            score: '10'
-          }
+        popcorn: [{
+          ranking: 1,
+          id: 'Devon Lane',
+          score: '100'
+        },
+        {
+          ranking: 2,
+          id: 'Cody Fisher',
+          score: '90'
+        },
+        {
+          ranking: 3,
+          id: 'Jacob Jones',
+          score: '80'
+        },
+        {
+          ranking: 4,
+          id: 'Robert Fox',
+          score: '75'
+        },
+        {
+          ranking: 5,
+          id: 'Eleanor Pena',
+          score: '70'
+        },
+        {
+          ranking: 6,
+          id: 'Kathryn Murphy',
+          score: '50'
+        },
+        {
+          ranking: 687,
+          id: 'John Doe',
+          score: '10'
+        }
         ],
-        moviz: [
-          {
-            ranking: 1,
-            id: 'Cody Fisher',
-            score: '100'
-          },
-          {
-            ranking: 2,
-            id: 'Devon Lane',
-            score: '90'
-          },
-          {
-            ranking: 3,
-            id: 'Kathryn Murphy',
-            score: '85'
-          },
-          {
-            ranking: 4,
-            id: 'Jacob Jones',
-            score: '80'
-          },
-          {
-            ranking: 5,
-            id: 'Robert Fox',
-            score: '75'
-          },
-          {
-            ranking: 6,
-            id: 'Eleanor Pena',
-            score: '70'
-          },
-          {
-            ranking: 450,
-            id: 'John Doe',
-            score: '20'
-          }
+        moviz: [{
+          ranking: 1,
+          id: 'Cody Fisher',
+          score: '100'
+        },
+        {
+          ranking: 2,
+          id: 'Devon Lane',
+          score: '90'
+        },
+        {
+          ranking: 3,
+          id: 'Kathryn Murphy',
+          score: '85'
+        },
+        {
+          ranking: 4,
+          id: 'Jacob Jones',
+          score: '80'
+        },
+        {
+          ranking: 5,
+          id: 'Robert Fox',
+          score: '75'
+        },
+        {
+          ranking: 6,
+          id: 'Eleanor Pena',
+          score: '70'
+        },
+        {
+          ranking: 450,
+          id: 'John Doe',
+          score: '20'
+        }
         ],
-        wsfquiz: [
-          {
-            ranking: 1,
-            id: 'Devon Lane',
-            score: '100'
-          },
-          {
-            ranking: 2,
-            id: 'Cody Fisher',
-            score: '90'
-          },
-          {
-            ranking: 3,
-            id: 'Jacob Jones',
-            score: '80'
-          },
-          {
-            ranking: 4,
-            id: 'Robert Fox',
-            score: '75'
-          },
-          {
-            ranking: 5,
-            id: 'Eleanor Pena',
-            score: '70'
-          },
-          {
-            ranking: 6,
-            id: 'Kathryn Murphy',
-            score: '60'
-          },
-          {
-            ranking: 80,
-            id: 'John Doe',
-            score: '40'
-          }
+        wsfquiz: [{
+          ranking: 1,
+          id: 'Devon Lane',
+          score: '100'
+        },
+        {
+          ranking: 2,
+          id: 'Cody Fisher',
+          score: '90'
+        },
+        {
+          ranking: 3,
+          id: 'Jacob Jones',
+          score: '80'
+        },
+        {
+          ranking: 4,
+          id: 'Robert Fox',
+          score: '75'
+        },
+        {
+          ranking: 5,
+          id: 'Eleanor Pena',
+          score: '70'
+        },
+        {
+          ranking: 6,
+          id: 'Kathryn Murphy',
+          score: '60'
+        },
+        {
+          ranking: 80,
+          id: 'John Doe',
+          score: '40'
+        }
         ],
-        cinequiz: [
-          {
-            ranking: 1,
-            id: 'Devon Lane',
-            score: '100'
-          },
-          {
-            ranking: 2,
-            id: 'Cody Fisher',
-            score: '90'
-          },
-          {
-            ranking: 3,
-            id: 'Jacob Jones',
-            score: '85'
-          },
-          {
-            ranking: 4,
-            id: 'Robert Fox',
-            score: '80'
-          },
-          {
-            ranking: 5,
-            id: 'Eleanor Pena',
-            score: '70'
-          },
-          {
-            ranking: 6,
-            id: 'Kathryn Murphy',
-            score: '65'
-          },
-          {
-            ranking: 125,
-            id: 'John Doe',
-            score: '30'
-          }
+        cinequiz: [{
+          ranking: 1,
+          id: 'Devon Lane',
+          score: '100'
+        },
+        {
+          ranking: 2,
+          id: 'Cody Fisher',
+          score: '90'
+        },
+        {
+          ranking: 3,
+          id: 'Jacob Jones',
+          score: '85'
+        },
+        {
+          ranking: 4,
+          id: 'Robert Fox',
+          score: '80'
+        },
+        {
+          ranking: 5,
+          id: 'Eleanor Pena',
+          score: '70'
+        },
+        {
+          ranking: 6,
+          id: 'Kathryn Murphy',
+          score: '65'
+        },
+        {
+          ranking: 125,
+          id: 'John Doe',
+          score: '30'
+        }
         ]
       }
     }
